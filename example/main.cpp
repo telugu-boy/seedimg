4a849633d04e9729a0d5808f5504395403e10340
telugu-boy <h0l33fuk@gmail.com>
2020-07-28 02:45:40 -0600

    tested for memory leak, there were none

--- example/main.cpp
+++ example/main.cpp
@@ -2,15 +2,18 @@
 #include <iostream>
 
 int main() {
-	auto a = seedimg::modules::png::from("violeur.png");
-	//bool b = seedimg::modules::png::to("seedviol.png", *a);
-	if (a != std::nullopt) {
-		seedimg::to("rawviol.seedimg", *a);
-		auto seedimage = seedimg::from("rawviol.seedimg");
-		if (seedimage != std::nullopt) {
-			seedimg::modules::png::to("seedviol.png", *seedimage);
+	{
+		auto a = seedimg::modules::png::from("violeur.png");
+		//bool b = seedimg::modules::png::to("seedviol.png", *a);
+		if (a != std::nullopt) {
+			seedimg::to("rawviol.seedimg", *a);
+			auto seedimage = seedimg::from("rawviol.seedimg");
+			if (seedimage != std::nullopt) {
+				seedimg::modules::png::to("seedviol.png", *seedimage);
+			}
+			// bool success = seedimg::modules::png::to("seedviol.png", a.value());
+			std::cout << (*a)->height << ' ' << (*a)->width << std::endl;
 		}
-		// bool success = seedimg::modules::png::to("seedviol.png", a.value());
-		std::cout << (*a)->height << ' ' << (*a)->width << std::endl;
 	}
+	std::cout << "done";
 }
