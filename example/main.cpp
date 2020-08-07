ca27998906f8179c84f3f2a6299c7ab73354675c
telugu-boy <42505125+telugu-boy@users.noreply.github.com>
2020-08-06 23:55:56 -0600

    added another test file

--- example/main.cpp
+++ example/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <filesystem>
+#include <iostream>
 
 #include <seedimg-autodetect/seedimg-autodetect.hpp>
 #include <seedimg-filters-core/seedimg-filters-core.hpp>
@@ -10,8 +10,8 @@ int main() {
   {
     auto a = seedimg_autodetect_from("violeur.png");
     if (a != std::nullopt) {
-      seedimg::filters::grayscale(*a, true);
-      bool b = seedimg_autodetect_to("boileur.jpg", *a);
+      // seedimg::filters::grayscale(*a, true);
+      bool b = seedimg_autodetect_to("boileur.png", *a);
     }
     std::cout << "done" << std::endl;
   }
