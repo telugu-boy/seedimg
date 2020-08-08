7a49e4417b6e3860d8a899a2bef6ab943f095b61
telugu-boy <h0l33fuk@gmail.com>
2020-08-08 02:13:16 -0600

    added tests for filters

--- example/main.cpp
+++ example/main.cpp
@@ -10,9 +10,9 @@ int main() {
   {
     auto a = seedimg_autodetect_from("violeur-int.png");
     if (a != std::nullopt) {
+      seedimg::filters::crop(*a, {50, 20}, {250, 183});
       seedimg::filters::grayscale(*a, true);
       seedimg::filters::invert(*a);
-      seedimg::filters::crop(*a, {50, 20}, {250, 183});
       bool b = seedimg_autodetect_to("bioleur.jpg", *a);
     }
     std::cout << "done" << std::endl;
---
+++ tests/test-filters-core.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <filesystem>
+#include <functional>
+#include <iostream>
+#include <unordered_map>
+
+#include <seedimg-autodetect/seedimg-autodetect.hpp>
+#include <seedimg-filters-core/seedimg-filters-core.hpp>
+
+enum class filter_functions { GRAYSCALE, INVERT, CROP };
+static const std::unordered_map<std::string, filter_functions> filter_mapping =
+    {{"grayscale", filter_functions::GRAYSCALE},
+     {"invert", filter_functions::INVERT},
+     {"crop", filter_functions::CROP}};
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cout << "FAIL";
+    exit(1);
+  }
+  std::cout << argv[1] << std::endl;
+  std::string res_dir = "tests_output/filters/";
+  std::filesystem::create_directories(res_dir);
+  auto img = *seedimg_autodetect_from("violeur.png");
+  switch (filter_mapping.at(argv[1])) {
+  case filter_functions::GRAYSCALE:
+    seedimg::filters::grayscale(img);
+    break;
+  case filter_functions::INVERT:
+    seedimg::filters::invert(img);
+    break;
+  case filter_functions::CROP:
+    seedimg::filters::crop(img, {50, 25}, {250, 183});
+    break;
+  }
+  char name_buf[256];
+  std::snprintf(name_buf, 256, "tests_output/filters/result-%s.png", argv[1]);
+  seedimg_autodetect_to(name_buf, img);
+  std::cout << "SUCCESS";
+}
