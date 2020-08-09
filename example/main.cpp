a8448ce1f6e55e975a914d35b78213077b072235
Santanu <bsantanu381@gmail.com>
2020-08-09 14:23:20 +0530

    Testing for Farbfeld.

--- example/main.cpp
+++ example/main.cpp
@@ -1,19 +1,25 @@
-#include <filesystem>
+﻿#include <filesystem>
 #include <iostream>
 
 #include <seedimg-autodetect/seedimg-autodetect.hpp>
 #include <seedimg-filters-core/seedimg-filters-core.hpp>
+
+#include <seedimg-format-farbfeld/farbfeld.hxx>
 
 int main() {
   std::cout << "Current path is " << std::filesystem::current_path()
             << std::endl;
   {
     auto a = seedimg_autodetect_from("violeur.png");
+    // auto a = seedimg::modules::farbfeld::from("boileur.ff");
     if (a != std::nullopt) {
-      seedimg::filters::grayscale(*a, true);
-      seedimg::filters::invert(*a);
-      seedimg::filters::crop(*a, {50, 20}, {250, 183});
-      bool b = seedimg_autodetect_to("bioleur.jpg", *a);
+      // seedimg::filters::grayscale(*a, true);
+      // seedimg::filters::invert(*a);
+      // seedimg::filters::crop(*a, {50, 20}, {250, 183});
+      // seedimg_autodetect_to("bioleur.jpg", *a);
+
+      // seedimg::modules::farbfeld::to("boileur_1.ff", *a);
+      seedimg::modules::farbfeld::to("boileur.ff", *a);
     }
     std::cout << "done" << std::endl;
   }
