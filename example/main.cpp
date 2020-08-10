0caba1a3055a8ac485d80ad98b972175e724b171
telugu-boy <h0l33fuk@gmail.com>
2020-08-09 21:54:00 -0600

    fixed grayscaling and invert algos

--- example/main.cpp
+++ example/main.cpp
@@ -9,9 +9,9 @@ int main() {
   std::cout << "Current path is " << std::filesystem::current_path()
             << std::endl;
   {
-    auto a = seedimg_autodetect_from("violeur-int.png");
+    auto a = seedimg_autodetect_from("violeur.png");
     if (a != nullptr) {
-      a->crop({50, 20}, {250, 183});
+      // a->crop({50, 20}, {250, 183});
       seedimg::filters::grayscale(a, true);
       seedimg::filters::invert(a);
       bool b = seedimg_autodetect_to("bioleur.jpg", a);
--- seedimg-filters-core/seedimg-filters-core.cpp
+++ seedimg-filters-core/seedimg-filters-core.cpp
@@ -22,7 +22,7 @@ void grayscale(std::unique_ptr<seedimg::img> &inp_img,
   if (lightness) {
     for (int y = 0; y < inp_img->height(); ++y) {
       for (int x = 0; x < inp_img->width(); ++x) {
-        auto pix = inp_img->pixel(x, y);
+        seedimg::pixel &pix = inp_img->pixel(x, y);
         uint8_t linear = static_cast<uint8_t>((0.2126 * (pix.r / 255.0) +
                                                0.7152 * (pix.g / 255.0) +
                                                0.0722 * (pix.b / 255.0)) *
@@ -33,7 +33,7 @@ void grayscale(std::unique_ptr<seedimg::img> &inp_img,
   } else {
     for (int y = 0; y < inp_img->height(); ++y) {
       for (int x = 0; x < inp_img->width(); ++x) {
-        auto pix = inp_img->pixel(x, y);
+        seedimg::pixel &pix = inp_img->pixel(x, y);
         uint8_t avg = (pix.r + pix.g + pix.b) / 3;
         pix = {avg, avg, avg, pix.a};
       }
@@ -46,7 +46,7 @@ void invert(std::unique_ptr<seedimg::img> &inp_img,
   if (invert_alpha) {
     for (int y = 0; y < inp_img->height(); ++y) {
       for (int x = 0; x < inp_img->width(); ++x) {
-        auto pix = inp_img->pixel(x, y);
+        seedimg::pixel &pix = inp_img->pixel(x, y);
         pix = {
             static_cast<std::uint8_t>(seedimg::img::MAX_PIXEL_VALUE - pix.r),
             static_cast<std::uint8_t>(seedimg::img::MAX_PIXEL_VALUE - pix.g),
@@ -57,7 +57,7 @@ void invert(std::unique_ptr<seedimg::img> &inp_img,
   } else {
     for (int y = 0; y < inp_img->height(); ++y) {
       for (int x = 0; x < inp_img->width(); ++x) {
-        auto pix = inp_img->pixel(x, y);
+        seedimg::pixel &pix = inp_img->pixel(x, y);
         pix = {static_cast<std::uint8_t>(seedimg::img::MAX_PIXEL_VALUE - pix.r),
                static_cast<std::uint8_t>(seedimg::img::MAX_PIXEL_VALUE - pix.g),
                static_cast<std::uint8_t>(seedimg::img::MAX_PIXEL_VALUE - pix.b),
