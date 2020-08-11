91da9ef3e5cce89880a904641ca00cc7fe362261
telugu-boy <h0l33fuk@gmail.com>
2020-08-11 01:55:00 -0600

    refactored filters to their own files

---
+++ seedimg-filters-core/blur.cpp
@@ -0,0 +1,12 @@
+#include "seedimg-filters-core.hpp"
+#include <cmath>
+#include <cstdint>
+#include <memory>
+#include <seedimg/seedimg.hpp>
+#include <thread>
+#include <vector>
+
+namespace seedimg::filters {
+void blur(std::unique_ptr<seedimg::img> &inp_img,
+          std::uint8_t blur_level) noexcept {}
+} // namespace seedimg::filters
