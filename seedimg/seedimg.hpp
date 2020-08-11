03e21161af87fef7e712d963dfe56c3f07fbd3ad
telugu-boy <h0l33fuk@gmail.com>
2020-08-11 03:11:28 -0600

    changed asserts to if statemnets

--- seedimg/seedimg.hpp
+++ seedimg/seedimg.hpp
@@ -37,21 +37,25 @@ public:
   img(simg_int w, simg_int h) : width_(w), height_(h) {
     data_ = reinterpret_cast<seedimg::pixel **>(std::malloc(
         static_cast<std::size_t>(height_) * sizeof(seedimg::pixel *)));
-    assert(data_ != nullptr);
+    if (data_ == nullptr)
+      exit(0);
     for (simg_int r = 0; r < height_; ++r) {
       data_[r] = reinterpret_cast<seedimg::pixel *>(std::malloc(
           static_cast<std::size_t>(width_) * sizeof(seedimg::pixel)));
-      assert(data_[r] != nullptr);
+      if (data_[r] == nullptr)
+        exit(0);
     }
   }
   img(seedimg::img const &img_) : width_(img_.width_), height_(img_.height_) {
     data_ = reinterpret_cast<seedimg::pixel **>(std::malloc(
         static_cast<std::size_t>(height_) * sizeof(seedimg::pixel *)));
-    assert(data_ != nullptr);
+    if (data_ == nullptr)
+      exit(0);
     for (simg_int r = 0; r < height_; ++r) {
       data_[r] = reinterpret_cast<seedimg::pixel *>(std::malloc(
           static_cast<std::size_t>(width_) * sizeof(seedimg::pixel)));
-      assert(data_[r] != nullptr);
+      if (data_[r] == nullptr)
+        exit(0);
     }
     for (simg_int y = 0; y < img_.height_; ++y) {
       std::memcpy(this->data_[y], img_.data_[y],
