b0981c11ed735b8106214d807983c6d35e587f1a
telugu-boy <h0l33fuk@gmail.com>
2020-08-13 03:51:49 -0600

    clang format entire project

--- seedimg-filters-core/grayscale.cpp
+++ seedimg-filters-core/grayscale.cpp
@@ -1,20 +1,20 @@
-/*********************************************************************** 
-seedimg - module based image manipulation library written in modern C++ 
-Copyright (C) 2020 telugu-boy 
- 
-This program is free software: you can redistribute it and/or modify 
-it under the terms of the GNU Lesser General Public License as published by 
-the Free Software Foundation, either version 3 of the License, or 
-(at your option) any later version. 
- 
-This program is distributed in the hope that it will be useful, 
-but WITHOUT ANY WARRANTY; without even the implied warranty of 
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
-GNU Lesser General Public License for more details. 
- 
-You should have received a copy of the GNU Lesser General Public License 
-along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-************************************************************************/ 
+/***********************************************************************
+seedimg - module based image manipulation library written in modern C++
+Copyright (C) 2020 telugu-boy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+************************************************************************/
 #include "seedimg-filters-core.hpp"
 #include <cstddef>
 #include <memory>
@@ -23,7 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <vector>
 
 void grayscale_worker_luminosity(std::unique_ptr<seedimg::img> &inp_img,
-                                 simg_int start_row, simg_int end_row) noexcept {
+                                 simg_int start_row,
+                                 simg_int end_row) noexcept {
   simg_int w = inp_img->width();
   for (; start_row < end_row; ++start_row) {
     for (simg_int x = 0; x < w; ++x) {
