8e803c24b1d3f0fb35cb65634d516f905c2bbb03
telugu-boy <h0l33fuk@gmail.com>
2020-08-14 01:44:11 -0600

    ifdef to ifndef

--- seedimg-autodetect/seedimg-autodetect.hpp
+++ seedimg-autodetect/seedimg-autodetect.hpp
@@ -16,7 +16,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ************************************************************************/
-#ifdef SEEDIMG_AUTODETECT_H
+#ifndef SEEDIMG_AUTODETECT_H
 #define SEEDIMG_AUTODETECT_H
 
 #include <memory>
