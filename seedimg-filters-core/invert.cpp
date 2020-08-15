dbfa0562e13042d48218194f44ff8954be09659f
telugu-boy <h0l33fuk@gmail.com>
2020-08-15 00:48:48 -0600

    removed more unnecessary code

--- seedimg-filters-core/invert.cpp
+++ seedimg-filters-core/invert.cpp
@@ -68,45 +68,6 @@ void invert_worker_alpha_only(std::unique_ptr<seedimg::img> &inp_img,
   }
 }
 
-void invert_i_worker(std::unique_ptr<seedimg::img> &inp_img, simg_int start_row,
-                     simg_int end_row) noexcept {
-  simg_int w = inp_img->width();
-  for (; start_row < end_row; ++start_row) {
-    for (simg_int x = 0; x < w; ++x) {
-      seedimg::pixel &pix = inp_img->pixel(x, start_row);
-      pix = {static_cast<std::uint8_t>(seedimg::img::MAX_PIXEL_VALUE - pix.r),
-             static_cast<std::uint8_t>(seedimg::img::MAX_PIXEL_VALUE - pix.g),
-             static_cast<std::uint8_t>(seedimg::img::MAX_PIXEL_VALUE - pix.b),
-             pix.a};
-    }
-  }
-}
-
-void invert_i_worker_alpha(std::unique_ptr<seedimg::img> &inp_img,
-                           simg_int start_row, simg_int end_row) noexcept {
-  simg_int w = inp_img->width();
-  for (; start_row < end_row; ++start_row) {
-    for (simg_int x = 0; x < w; ++x) {
-      seedimg::pixel &pix = inp_img->pixel(x, start_row);
-      pix = {static_cast<std::uint8_t>(seedimg::img::MAX_PIXEL_VALUE - pix.r),
-             static_cast<std::uint8_t>(seedimg::img::MAX_PIXEL_VALUE - pix.g),
-             static_cast<std::uint8_t>(seedimg::img::MAX_PIXEL_VALUE - pix.b),
-             static_cast<std::uint8_t>(seedimg::img::MAX_PIXEL_VALUE - pix.a)};
-    }
-  }
-}
-
-void invert_i_worker_alpha_only(std::unique_ptr<seedimg::img> &inp_img,
-                                simg_int start_row, simg_int end_row) noexcept {
-  simg_int w = inp_img->width();
-  for (; start_row < end_row; ++start_row) {
-    for (simg_int x = 0; x < w; ++x) {
-      seedimg::pixel &pix = inp_img->pixel(x, start_row);
-      pix.a = static_cast<std::uint8_t>(seedimg::img::MAX_PIXEL_VALUE - pix.a);
-    }
-  }
-}
-
 namespace seedimg::filters {
 
 void invert(std::unique_ptr<seedimg::img> &inp_img,
@@ -144,34 +105,11 @@ void invert_a(std::unique_ptr<seedimg::img> &inp_img,
 };
 
 void invert_i(std::unique_ptr<seedimg::img> &inp_img) {
-  auto start_end = inp_img->start_end_rows();
-  std::vector<std::thread> workers(start_end.size());
-  for (std::size_t i = 0; i < workers.size(); i++) {
-    workers.at(i) = std::thread(invert_i_worker, std::ref(inp_img),
-                                start_end.at(i).first, start_end.at(i).second);
-  }
-  for (std::size_t i = 0; i < workers.size(); ++i)
-    workers.at(i).join();
+  invert(inp_img, inp_img);
 }
 
 void invert_a_i(std::unique_ptr<seedimg::img> &inp_img,
                 bool invert_alpha_only) {
-  auto start_end = inp_img->start_end_rows();
-  std::vector<std::thread> workers(start_end.size());
-  if (invert_alpha_only) {
-    for (std::size_t i = 0; i < workers.size(); i++) {
-      workers.at(i) =
-          std::thread(invert_i_worker_alpha_only, std::ref(inp_img),
-                      start_end.at(i).first, start_end.at(i).second);
-    }
-  } else {
-    for (std::size_t i = 0; i < workers.size(); i++) {
-      workers.at(i) =
-          std::thread(invert_i_worker_alpha, std::ref(inp_img),
-                      start_end.at(i).first, start_end.at(i).second);
-    }
-  }
-  for (std::size_t i = 0; i < workers.size(); ++i)
-    workers.at(i).join();
+  invert_a(inp_img, inp_img, invert_alpha_only);
 }
 } // namespace seedimg::filters
