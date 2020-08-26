93f6b56698bc436d875f37316202720d62eaa46e
telugu-boy <h0l33fuk@gmail.com>
2020-08-26 01:43:28 -0600

    changed to conform to odr

--- include/seedimg.hpp
+++ include/seedimg.hpp
@@ -50,80 +50,25 @@ struct pixel {
 class img {
 public:
   static constexpr std::uint8_t MAX_PIXEL_VALUE = UINT8_MAX;
-  img() : width_(0), height_(0), data_(nullptr) {}
-  img(simg_int w, simg_int h) : width_{w}, height_{h} {
-    data_ = reinterpret_cast<seedimg::pixel *>(std::malloc(
-        static_cast<std::size_t>(height_ * width_) * sizeof(seedimg::pixel *)));
-  }
-  img(simg_int w, simg_int h, seedimg::pixel *u_data)
-      : width_{w}, height_{h}, data_{u_data} {}
-  img(seedimg::img const &img_) : img(img_.width_, img_.height_) {
-    std::memcpy(this->data_, img_.data_,
-                static_cast<std::size_t>(img_.width_ * img_.height_) *
-                    sizeof(seedimg::pixel));
-  }
-  img(seedimg::img &&other) : width_{0}, height_{0}, data_{nullptr} {
-    this->width_ = other.width_;
-    this->height_ = other.height_;
-    this->data_ = other.data_;
-    other.width_ = 0;
-    other.height_ = 0;
-    other.data_ = nullptr;
-  }
+  img();
+  img(simg_int w, simg_int h);
+  img(simg_int w, simg_int h, seedimg::pixel *u_data);
+  img(seedimg::img const &img_);
+  img(seedimg::img &&other);
 
-  ~img() {
-    if (data_ != nullptr) {
-      std::free(data_);
-    }
-  }
+  ~img();
 
-  std::vector<std::pair<simg_int, simg_int>> start_end_rows() {
-    std::vector<std::pair<simg_int, simg_int>> res;
-    auto processor_count =
-        std::min(this->height(),
-                 static_cast<simg_int>(std::thread::hardware_concurrency()));
-    if (processor_count == 0)
-      processor_count = 1;
-    res.reserve(static_cast<std::size_t>(processor_count));
-    simg_int rows_per_thread = this->height() / processor_count;
-    for (simg_int i = 0; i < processor_count * rows_per_thread;
-         i += rows_per_thread)
-      res.emplace_back(i, i + rows_per_thread);
-    res[res.size() - 1].second += this->height() % processor_count;
-    return res;
-  }
+  std::vector<std::pair<simg_int, simg_int>> start_end_rows();
 
-  std::vector<std::pair<simg_int, simg_int>> start_end_cols() {
-    std::vector<std::pair<simg_int, simg_int>> res;
-    auto processor_count =
-        std::min(this->width(),
-                 static_cast<simg_int>(std::thread::hardware_concurrency()));
-    if (processor_count == 0)
-      processor_count = 1;
-    res.reserve(static_cast<std::size_t>(processor_count));
-    simg_int cols_per_thread = this->width() / processor_count;
-    for (simg_int i = 0; i < processor_count * cols_per_thread;
-         i += cols_per_thread)
-      res.emplace_back(i, i + cols_per_thread);
-    res[res.size() - 1].second += this->width() % processor_count;
-    return res;
-  }
+  std::vector<std::pair<simg_int, simg_int>> start_end_cols();
 
-  seedimg::pixel &pixel(simg_int x, simg_int y) noexcept {
-    return data_[y * this->width_ + x];
-  }
-  seedimg::pixel &pixel(seedimg::point p) noexcept {
-    return pixel(p.first, p.second);
-  }
-  seedimg::pixel &pixel(simg_int x) {
-    if (x > this->width() * this->height() - 1)
-      std::terminate();
-    return pixel(x / this->width(), x % this->width());
-  }
-  seedimg::pixel *row(simg_int y) noexcept { return data_ + y * this->width_; }
-  seedimg::pixel *data() noexcept { return data_; }
-  simg_int width() noexcept { return width_; }
-  simg_int height() noexcept { return height_; }
+  seedimg::pixel &pixel(simg_int x, simg_int y) noexcept;
+  seedimg::pixel &pixel(seedimg::point p) noexcept;
+  seedimg::pixel &pixel(simg_int x);
+  seedimg::pixel *row(simg_int y) noexcept;
+  seedimg::pixel *data() noexcept;
+  simg_int width() noexcept;
+  simg_int height() noexcept;
 
 private:
   simg_int width_;
--- src/seedimg/seedimg.cpp
+++ src/seedimg/seedimg.cpp
@@ -30,6 +30,84 @@
 #include <seedimg.hpp>
 
 namespace seedimg {
+
+img::img() : width_(0), height_(0), data_(nullptr) {}
+img::img(simg_int w, simg_int h) : width_{w}, height_{h} {
+  data_ = reinterpret_cast<seedimg::pixel *>(std::malloc(
+      static_cast<std::size_t>(height_ * width_) * sizeof(seedimg::pixel *)));
+}
+img::img(simg_int w, simg_int h, seedimg::pixel *u_data)
+    : width_{w}, height_{h}, data_{u_data} {}
+img::img(seedimg::img const &img_) : img(img_.width_, img_.height_) {
+  std::memcpy(this->data_, img_.data_,
+              static_cast<std::size_t>(img_.width_ * img_.height_) *
+                  sizeof(seedimg::pixel));
+}
+img::img(seedimg::img &&other) : width_{0}, height_{0}, data_{nullptr} {
+  this->width_ = other.width_;
+  this->height_ = other.height_;
+  this->data_ = other.data_;
+  other.width_ = 0;
+  other.height_ = 0;
+  other.data_ = nullptr;
+}
+
+img::~img() {
+  if (data_ != nullptr) {
+    std::free(data_);
+  }
+}
+
+std::vector<std::pair<simg_int, simg_int>> img::start_end_rows() {
+  std::vector<std::pair<simg_int, simg_int>> res;
+  auto processor_count =
+      std::min(this->height(),
+               static_cast<simg_int>(std::thread::hardware_concurrency()));
+  if (processor_count == 0)
+    processor_count = 1;
+  res.reserve(static_cast<std::size_t>(processor_count));
+  simg_int rows_per_thread = this->height() / processor_count;
+  for (simg_int i = 0; i < processor_count * rows_per_thread;
+       i += rows_per_thread)
+    res.emplace_back(i, i + rows_per_thread);
+  res[res.size() - 1].second += this->height() % processor_count;
+  return res;
+}
+
+std::vector<std::pair<simg_int, simg_int>> img::start_end_cols() {
+  std::vector<std::pair<simg_int, simg_int>> res;
+  auto processor_count =
+      std::min(this->width(),
+               static_cast<simg_int>(std::thread::hardware_concurrency()));
+  if (processor_count == 0)
+    processor_count = 1;
+  res.reserve(static_cast<std::size_t>(processor_count));
+  simg_int cols_per_thread = this->width() / processor_count;
+  for (simg_int i = 0; i < processor_count * cols_per_thread;
+       i += cols_per_thread)
+    res.emplace_back(i, i + cols_per_thread);
+  res[res.size() - 1].second += this->width() % processor_count;
+  return res;
+}
+
+seedimg::pixel &img::pixel(simg_int x, simg_int y) noexcept {
+  return data_[y * this->width_ + x];
+}
+seedimg::pixel &img::pixel(seedimg::point p) noexcept {
+  return pixel(p.first, p.second);
+}
+seedimg::pixel &img::pixel(simg_int x) {
+  if (x > this->width() * this->height() - 1)
+    std::terminate();
+  return pixel(x / this->width(), x % this->width());
+}
+seedimg::pixel *img::row(simg_int y) noexcept {
+  return data_ + y * this->width_;
+}
+seedimg::pixel *img::data() noexcept { return data_; }
+simg_int img::width() noexcept { return width_; }
+simg_int img::height() noexcept { return height_; }
+
 std::unique_ptr<seedimg::img> make(simg_int width, simg_int height) {
   return std::make_unique<seedimg::img>(width, height);
 }
