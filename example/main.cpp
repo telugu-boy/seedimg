ba66b31fe4f50923bfef242c70390e980d9870c2
telugu-boy <h0l33fuk@gmail.com>
2020-08-30 00:16:18 -0600

    made opencl optional

--- example/main.cpp
+++ example/main.cpp
@@ -21,27 +21,27 @@ seedimg - module based image manipulation library written in modern
 
 #include <seedimg-autodetect.hpp>
 #include <seedimg-filters/seedimg-filters-core.hpp>
-#include <seedimg-filters/seedimg-filters-ocl.hpp>
+// #include <seedimg-filters/seedimg-filters-ocl.hpp>
 
 int main() {
   using namespace seedimg::filters;
   std::cout << "Current path is " << std::filesystem::current_path()
             << std::endl;
   {
-    ocl::init_ocl_singleton(1, 0);
+    // ocl::init_ocl_singleton(1, 0);
     auto a = seedimg::load("cat.png");
     auto res_img = seedimg::make(a->width(), a->height());
     if (a != nullptr) {
       // crop_i(a, {122, 166}, {244, 332});
       // grayscale_i(a, true);
       // invert_i(a);
-      // blur_i(a, 10);
+      blur_i(a, 10);
       // h_blur_i(a, 10);
       // v_blur_i(a, 100, 1);
       // convolution(a, {{-1, 1, -1}, {-2, 4, -2}, {-1, 1, -1}});
       // rotate_hue_i(a, 180);
       // h_mirror_i(a);
-      ocl::rotate_hue_i(a, -90);
+      // ocl::rotate_hue_i(a, -90);
       seedimg::save("biol.jpg", a);
       // bool b = seedimg::modules::jpeg::to("biol.jpg", a, 1);
     } else {
