cb11027353935a160b87d2e413429f683ecf968b
Santanu <bsantanu381@gmail.com>
2020-09-13 17:54:52 +0530

    Minoeur changes

--- src/seedimg-utils/seedimg-utils.cpp
+++ src/seedimg-utils/seedimg-utils.cpp
@@ -23,7 +23,7 @@
 #include <seedimg.hpp>
 
 namespace seedimg::utils {
-histogram_result histogram(std::shared_ptr<seedimg::img> &input) {
+histogram_result histogram(simg& input) {
   histogram_result result;
 
   for (simg_int y = 0; y < input->height(); ++y) {
