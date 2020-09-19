bc37c19693f2704ddce25fb949e097a994de2f51
telugu-boy <42505125+telugu-boy@users.noreply.github.com>
2020-09-19 16:56:27 -0600

    remove unnecessary definitions

--- src/seedimg-filters/seedimg-filters-core/apply-mat.cpp
+++ src/seedimg-filters/seedimg-filters-core/apply-mat.cpp
@@ -58,29 +58,6 @@ void apply_mat_lut_worker(simg &inp_img, simg &res_img, simg_int start,
 }
 
 namespace seedimg::filters {
-constexpr fsmat scalar_mat_mul(const fsmat &mat, float sc) {
-  fsmat res{};
-  for (std::size_t i = 0; i < 16; i++) {
-    res[i] = mat[i] * sc;
-  }
-  return res;
-}
-
-constexpr smat scalar_mat_mul(const smat &mat, float sc) {
-  smat res{};
-  for (std::size_t i = 0; i < 9; i++) {
-    res[i] = mat[i] * sc;
-  }
-  return res;
-}
-
-constexpr fsmat to_fsmat(const smat &mat) {
-  return fsmat{
-      mat[0], mat[1], mat[2], 0.0f, mat[3], mat[4], mat[5], 0.0f,
-      mat[6], mat[7], mat[8], 0.0f, 0.0f,   0.0f,   0.0f,   1.0f,
-  };
-}
-
 smat generate_hue_mat(float angle) {
   const float sinr = static_cast<float>(std::sin(angle * PI / 180));
   const float cosr = static_cast<float>(std::cos(angle * PI / 180));
