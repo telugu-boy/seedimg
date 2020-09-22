2089ac90f1b7d812f394ceb08a0ab4c102044dc3
Santanu <bsantanu381@gmail.com>
2020-09-22 13:38:54 +0530

    Column iterator
    Kinda exclusively made for seedimg::filters::resize_bilin() for the column pass stage.
    Though this should be generalised to a pointer, supporting all the operations.

--- include/seedimg-utils.hpp
+++ include/seedimg-utils.hpp
@@ -60,5 +60,49 @@ void vrt_thread(T &&func, simg &inp_img, simg &res_img, Args &&... args) {
     worker.join();
   }
 }
+
+/**
+ * @brief Column-major iteration. As the buffer internally is row-major,
+ * and at certain conditions one requires column-major ordering.
+ *
+ * @note Not all features are implemented, it is exclusively built for
+ * the seedimg::extras::resize_bilin function.
+ */
+class column_iterator {
+private:
+    simg&       img;
+    std::size_t col;
+//    std::size_t pos;
+public:
+    column_iterator(simg& img, std::size_t col = 0) :
+        img(img), col(col) {}
+
+//    seedimg::pixel& operator* () noexcept {
+//        // formula for indexing pixels in column major order is:
+//        //    index = height * pos + col
+//        return img->data()[img->height()*pos + col];
+//    }
+//
+//    seedimg::pixel* operator++() noexcept {
+//        // pos is bounded to [0..height), when it overflows
+//        // increment 'col' and reset 'pos' to zero.
+//        if(++pos == img->height())
+//            (void)++col, pos = 0;
+//        return img->data();
+//    }
+//
+//    seedimg::pixel* operator--() noexcept {
+//        if(pos == 0)
+//            (void)--col, pos = img->height() - 1;
+//        else --pos;
+//        return img->data();
+//    }
+
+    seedimg::pixel& operator[](std::size_t i) const noexcept {
+        return img->data()[img->width()*i + col];
+    }
+    void set_column(std::size_t c) noexcept { col = c; }
+};
+
 } // namespace seedimg::utils
 #endif
