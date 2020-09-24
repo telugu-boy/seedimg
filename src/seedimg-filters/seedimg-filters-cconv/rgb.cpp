8ae249a2c666456f589e43f872766960f98acec2
telugu-boy <h0l33fuk@gmail.com>
2020-09-24 03:53:36 -0600

    copy pasted constexpr mat decls

--- src/seedimg-filters/seedimg-filters-cconv/rgb.cpp
+++ src/seedimg-filters/seedimg-filters-cconv/rgb.cpp
@@ -18,12 +18,32 @@
 
 #include <cmath>
 #include <seedimg-filters/seedimg-filters-cconv.hpp>
+#include <seedimg-filters/seedimg-filters-core.hpp>
 #include <seedimg-utils.hpp>
 
 // resource headers. this is for the rgb->ycbcr_jpeg
 #include "from_ycbcr_jpeg_lut.rh"
 // and this is for bt601
 #include "from_ycbcr_bt601_lut.rh"
+
+static constexpr seedimg::smat const ycbcr_jpeg_mat = {
+    0.299f,     -0.168736f, 0.5f, 0.587f,    -0.331264f,
+    -0.418688f, 0.114f,     0.5f, -0.081312f};
+
+static constexpr seedimg::lutvec const ycbcr_jpeg_vec = {0, 128, 128};
+
+static constexpr seedimg::slut<seedimg::smat> const ycbcr_jpeg_lut =
+    seedimg::utils::gen_lut(ycbcr_jpeg_mat);
+
+static constexpr seedimg::smat const ycbcr_bt601_mat = {
+    65.738f / 256.0f,  -37.945f / 256.0f, 112.439f / 256.0f,
+    129.057f / 256.0f, -74.494f / 256.0f, -94.154f / 256.0f,
+    25.064f / 256.0f,  112.439f / 256.0f, -18.285f / 256.0f};
+
+static constexpr seedimg::lutvec const ycbcr_bt601_vec = {16, 128, 128};
+
+static constexpr seedimg::slut<seedimg::smat> const ycbcr_bt601_lut =
+    seedimg::utils::gen_lut(ycbcr_bt601_mat);
 
 void hsv2rgb_worker(simg &inp_img, simg &res_img, simg_int start,
                     simg_int end) {
