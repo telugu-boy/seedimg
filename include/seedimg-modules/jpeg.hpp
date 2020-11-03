357810a9c2f3f6c13d71dd5376644ba13fa78ee4
tripulse <bsantanu381@gmail.com>
2020-11-03 10:33:47 +0530

    jpeg stream encoding support

--- include/seedimg-modules/jpeg.hpp
+++ include/seedimg-modules/jpeg.hpp
@@ -106,3 +106,97 @@ public:
 };
 }
 
+namespace seedimg::modules::encoding {
+enum jpeg_subsampling {
+    YCbCr_444,
+    YCbCr_422,
+    YCbCr_411,
+    YCbCr_420
+};
+
+struct jpeg_restarts {
+    bool per_row;
+    int  val;
+};
+
+class jpeg : public output_abc {
+    jpeg_compress_struct c;
+    seedimg_jpeg_error_mgr err;
+
+    struct ostream_jdstm {
+        jpeg_destination_mgr pub;
+
+        JOCTET* buffer;  // track of jpeg buffer start.
+    };
+
+    static void    init_destination   (j_compress_ptr j) {
+      auto dest = reinterpret_cast<ostream_jdstm*>(j->dest);
+
+      dest->buffer = reinterpret_cast<JOCTET*>(
+        (*j->mem->alloc_small) (reinterpret_cast<j_common_ptr>(j),
+                                JPOOL_IMAGE,
+                                J_BUF_SIZE * sizeof(JOCTET)));
+
+      dest->pub.next_output_byte = dest->buffer;
+      dest->pub.free_in_buffer   = J_BUF_SIZE;
+    }
+    static boolean empty_output_buffer(j_compress_ptr j) {
+      auto dest = reinterpret_cast<ostream_jdstm*>(j->dest);
+
+      try {
+        reinterpret_cast<jpeg*>(j->client_data)
+                  ->out.write(reinterpret_cast<char*>(dest->buffer), J_BUF_SIZE);
+      } catch(std::ios_base::failure) {
+          return FALSE;
+      }
+
+      dest->pub.next_output_byte = dest->buffer;
+      dest->pub.free_in_buffer   = J_BUF_SIZE;
+
+      return TRUE;
+    }
+    static void    term_destination   (j_compress_ptr j) {
+      auto dest = reinterpret_cast<ostream_jdstm*>(j->dest);
+      auto self = reinterpret_cast<jpeg*>(j->client_data);
+
+      try {
+        self->out.write(reinterpret_cast<char*>(dest->buffer),
+                        static_cast<std::streamsize>(J_BUF_SIZE - dest->pub.free_in_buffer));
+        self->out.flush();
+      } catch(std::ios_base::failure) {
+            j->err->msg_code   = JERR_FILE_WRITE;
+          (*j->err->error_exit) (reinterpret_cast<j_common_ptr>(j));
+      }
+    }
+
+    std::ostream& out;
+    simg_int      cur_scline = 0;
+public:
+    /**
+     * @param quality   IJG quality scale (0..100], though qualities below 25 yield
+     *                  the same result as 25 due to forced baseline constraint.
+     *
+     * @param sumbsamp  Subsampling ratio of luma and chroma channels.
+     * @param optimize  Generate optimistic huffman tables for better compression.
+     * @param arith     Use compression-wise superior arithmetic coding over Huffman.
+     * @param smoothing Smoothing of input image [0..100].
+     * @param method    DCT (discrete cosine transform) compuation method to use.
+     * @param res_int   Restart interval of MCU blocks or rows.
+     */
+    jpeg(std::ostream& buf, simg_int         width,
+                            simg_int         height,
+                            int              quality     = 100,
+                            jpeg_subsampling subsamp     = YCbCr_444,
+                            bool             progressive = false,
+                            bool             optimize    = false,
+                            bool             arithcoding = false,
+                            int              smoothing   = 0,
+                            J_DCT_METHOD     dct_method  = JDCT_DEFAULT,
+                            jpeg_restarts    res_int     = {false, 0});
+
+    bool write(const pixel* const from);
+    void flush();
+    ~jpeg();
+};
+}
+#endif
--- src/seedimg-modules/seedimg-jpeg/jpeg.cpp
+++ src/seedimg-modules/seedimg-jpeg/jpeg.cpp
@@ -88,3 +88,118 @@ jpeg::~jpeg() {
 }
 
 
+namespace seedimg::modules::encoding {
+jpeg::jpeg(std::ostream& buf, simg_int         width,
+                              simg_int         height,
+                              int              quality,
+                              jpeg_subsampling subsamp,
+                              bool             progressive,
+                              bool             optimize,
+                              bool             arithcoding,
+                              int              smoothing,
+                              J_DCT_METHOD     dct_method,
+                              jpeg_restarts    res_int) : out(buf) {
+    c.err = jpeg_std_error(&err.pub);
+    err.pub.error_exit = jpeg_error_exit;
+
+    if(setjmp(err.buf)) {
+        jpeg_destroy_compress(&c);
+        throw output_failure{jpeg_last_error_msg};
+    }
+
+    jpeg_create_compress(&c);
+
+    c.client_data = this;
+    c.dest = reinterpret_cast<jpeg_destination_mgr*>(
+                c.mem->alloc_small(reinterpret_cast<j_common_ptr>(&c),
+                                   JPOOL_PERMANENT,
+                                   sizeof(ostream_jdstm)));
+
+    c.dest->init_destination    = init_destination;
+    c.dest->empty_output_buffer = empty_output_buffer;
+    c.dest->term_destination    = term_destination;
+
+    c.in_color_space   = JCS_EXT_RGBA;
+    c.input_components = 4;
+
+    jpeg_set_defaults(&c);
+
+    c.image_width      = static_cast<JDIMENSION>(width);
+    c.image_height     = static_cast<JDIMENSION>(height);
+
+    jpeg_set_quality(&c, quality, TRUE);
+
+    switch(subsamp) {
+        case YCbCr_444:  // all planes have same resolution.
+            c.comp_info[0].h_samp_factor = 1;
+            c.comp_info[0].v_samp_factor = 1;
+            c.comp_info[1].h_samp_factor = 1;
+            c.comp_info[1].v_samp_factor = 1;
+            c.comp_info[2].h_samp_factor = 1;
+            c.comp_info[2].v_samp_factor = 1;
+        break;
+
+        case YCbCr_422:  // both chroma channels are halved horizonally.
+            c.comp_info[0].h_samp_factor = 2;
+            c.comp_info[0].v_samp_factor = 2;
+            c.comp_info[1].h_samp_factor = 1;
+            c.comp_info[1].v_samp_factor = 2;
+            c.comp_info[2].h_samp_factor = 1;
+            c.comp_info[2].v_samp_factor = 2;
+        break;
+
+        case YCbCr_411:  // both chroma channels are quartered horizontally.
+            c.comp_info[0].h_samp_factor = 4;
+            c.comp_info[0].v_samp_factor = 4;
+            c.comp_info[1].h_samp_factor = 1;
+            c.comp_info[1].v_samp_factor = 4;
+            c.comp_info[2].h_samp_factor = 1;
+            c.comp_info[2].v_samp_factor = 4;
+        break;
+
+        case YCbCr_420:  // both chroma channels are halved vertically and horizontally.
+            c.comp_info[0].h_samp_factor = 2;
+            c.comp_info[0].v_samp_factor = 2;
+            c.comp_info[1].h_samp_factor = 1;
+            c.comp_info[1].v_samp_factor = 1;
+            c.comp_info[2].h_samp_factor = 1;
+            c.comp_info[2].v_samp_factor = 1;
+        break;
+    }
+
+    if(progressive)
+        jpeg_simple_progression(&c);
+
+    c.optimize_coding  = optimize;
+    c.arith_code       = arithcoding;
+    c.smoothing_factor = smoothing;
+    c.dct_method       = dct_method;
+
+    if(res_int.per_row)
+        c.restart_in_rows = res_int.val;
+    else
+        c.restart_interval = seedimg::utils::clamp(
+                    res_int.val,
+                    0, std::numeric_limits<int>::max());
+
+    jpeg_start_compress(&c, TRUE);
+}
+
+bool jpeg::write(const pixel* from) {
+    if(cur_scline >= c.image_height)
+        return false;
+
+    auto row = reinterpret_cast<JSAMPROW>(const_cast<pixel*>(from));
+    if(jpeg_write_scanlines(&c, &row, 1) != 1)
+        return false;
+
+    ++cur_scline;
+    return true;
+}
+
+void jpeg::flush() { jpeg_finish_compress(&c); }
+jpeg::~jpeg() {
+    flush();  // ensure data is out.
+    jpeg_destroy_compress(&c);
+}
+}
