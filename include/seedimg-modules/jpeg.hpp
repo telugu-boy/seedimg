1395cbc5aedd3e5570d1c8540572dd0b9176b351
tripulse <bsantanu381@gmail.com>
2020-11-03 10:33:04 +0530

    jpeg stream decoding support

--- include/seedimg-modules/jpeg.hpp
+++ include/seedimg-modules/jpeg.hpp
@@ -38,3 +38,71 @@ struct seedimg_jpeg_error_mgr {
   struct jpeg_error_mgr pub;
   jmp_buf               buf;
 };
+
+namespace seedimg::modules::decoding {
+class jpeg : public input_abc {
+    seedimg_jpeg_error_mgr err;
+    struct istream_jsrcm {
+        jpeg_source_mgr pub;
+
+        JOCTET* buffer;     // track of jpeg buffer start.
+        bool    SOF;        // start-of-file indicator.
+    };
+
+    jpeg_decompress_struct d;
+    std::istream&          in;
+
+    static void    init_source       (j_decompress_ptr                  ) {}
+    static boolean fill_input_buffer (j_decompress_ptr j                ) {
+        auto self = reinterpret_cast<jpeg*>(j->client_data);
+        auto src = reinterpret_cast<istream_jsrcm*>(j->src);
+
+        try {
+            self->in.read(reinterpret_cast<char*>(src->buffer), J_BUF_SIZE);
+        } catch(std::ios_base::failure) {
+            return FALSE;   /* fatal IO error */
+        }
+        if(self->in.gcount() <= 0) {
+            if(src->SOF) {
+                j->err->msg_code   = JERR_INPUT_EMPTY;
+              (*j->err->error_exit) (reinterpret_cast<j_common_ptr>(j));
+            }
+
+            src->buffer[0] = 0xFF;
+            src->buffer[1] = JPEG_EOI;
+
+            src->pub.bytes_in_buffer = 2;
+        } else
+            src->pub.bytes_in_buffer = static_cast<std::size_t>(self->in.gcount());
+
+        src->SOF                 = false;
+        src->pub.next_input_byte = src->buffer;
+
+        return TRUE;
+    }
+    static void    skip_input_data   (j_decompress_ptr j, long num_bytes) {
+        if (num_bytes > 0) {
+            while (num_bytes > static_cast<long>(j->src->bytes_in_buffer)) {
+              num_bytes -= static_cast<long>(j->src->bytes_in_buffer);
+              (void) (*j->src->fill_input_buffer)(j);
+            }
+            j->src->next_input_byte += static_cast<size_t>(num_bytes);
+            j->src->bytes_in_buffer -= static_cast<size_t>(num_bytes);
+        }
+    }
+    static void    term_source       (j_decompress_ptr                  ) {}
+public:
+    /**
+     * @param method    DCT (discrete cosine transform) compuation method to use.
+     */
+    jpeg(std::istream& buf, J_DCT_METHOD method = JDCT_DEFAULT);
+
+    simg_int width()  const noexcept;
+    simg_int height() const noexcept;
+
+    bool read(pixel *to);
+
+    ~jpeg();
+};
+}
+
--- src/seedimg-modules/seedimg-jpeg/jpeg.cpp
+++ src/seedimg-modules/seedimg-jpeg/jpeg.cpp
@@ -26,3 +26,65 @@ static char jpeg_last_error_msg[JMSG_LENGTH_MAX];
   (*(j->err->format_message))(j, jpeg_last_error_msg);
   std::longjmp(err->buf, 1);
 }
+
+namespace seedimg::modules::decoding {
+jpeg::jpeg(std::istream& buf, J_DCT_METHOD method) : in(buf)
+{
+    d.err = jpeg_std_error(&err.pub);
+    err.pub.error_exit = jpeg_error_exit;
+
+    if(setjmp(err.buf)) {
+        jpeg_destroy_decompress(&d);
+        throw input_failure{jpeg_last_error_msg};
+    }
+
+    jpeg_create_decompress(&d);
+
+
+    auto src = reinterpret_cast<istream_jsrcm*>(
+                    d.mem->alloc_small(reinterpret_cast<j_common_ptr>(&d),
+                                       JPOOL_PERMANENT,
+                                       sizeof(istream_jsrcm)));
+    d.client_data = this;
+    d.src         = reinterpret_cast<jpeg_source_mgr*>(src);
+
+    src->buffer = reinterpret_cast<JOCTET*>(
+                            d.mem->alloc_small(reinterpret_cast<j_common_ptr>(&d),
+                                               JPOOL_PERMANENT,
+                                               J_BUF_SIZE * sizeof(JOCTET)));
+    src->pub.init_source       = init_source;
+    src->pub.fill_input_buffer = fill_input_buffer;
+    src->pub.skip_input_data   = skip_input_data;
+    src->pub.resync_to_restart = jpeg_resync_to_restart;
+    src->pub.term_source       = term_source;
+
+    src->pub.next_input_byte = nullptr;
+    src->pub.bytes_in_buffer = 0;
+    src->SOF                 = true;
+
+    jpeg_read_header(&d, FALSE);
+
+    d.out_color_space = JCS_EXT_RGBA;
+    d.dct_method      = method;
+
+    jpeg_start_decompress(&d);
+}
+
+simg_int jpeg::width()  const noexcept { return d.image_width;  }
+simg_int jpeg::height() const noexcept { return d.image_height; }
+
+bool jpeg::read(pixel* to) {
+    if(d.output_scanline >= d.output_height)
+        return false;
+
+    auto row = reinterpret_cast<JSAMPROW>(to);
+    return jpeg_read_scanlines(&d, &row, 1) == 1;
+}
+
+jpeg::~jpeg() {
+    jpeg_finish_decompress(&d);
+    jpeg_destroy_decompress(&d);
+}
+}
+
+
