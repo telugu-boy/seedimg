82055d3adb632d9b6b292757b0c5da391d7bfd69
somedude3483 <62268652+somedude3483@users.noreply.github.com>
2020-11-08 20:58:57 -0500

    Obunga
    make script great

--- scripts/genLUT_ycbcr.py
+++ scripts/genLUT_ycbcr.py
@@ -1,32 +1,55 @@
-def write_rh(l, typ, names, filename):
-    arr = []
-    for inner in range(len(l)):
-        arr.append(f"static constexpr {typ} const {names[inner]}[] = {{ {', '.join(map(str, l[inner]))} }};")
-        
-    with open("../include/seedimg-filters/rh/" + filename, "w") as f:
-        for s in arr:
-            f.write(s + "\n")
+import posixpath
 
-#ycbcr_jpeg2rgb
-#this will generate a .rh (resource header) in the respective folder, which can be included:
-#it will have the nine lookup tables needed for this operation.
-#which are: the nth row of the jpeg matrix * y, cb, cr of the pixel.
+
+jpeg_mat = [1.0, 0.0, 1.402, 1.0, -0.34414, -0.71414, 1.0, 1.772, 0.0]
+
+
+def write_rh(l, typ, names, filename):
+    array = []
+    for index, inner in enumerate(l):
+        array.append(
+            f"static constexpr {typ} const {names[index]}[] = {{ {', '.join(map(str, inner))} }};"
+        )
+
+    with open(posixpath.join("../include/seedimg-filters/rh", filename), "w") as file:
+        for s in array:
+            file.write(f"{s}\n")
+
+
 def gen_jpeg():
-    jpeg_mat = [1.0, 0.0, 1.402, 
-                1.0, -0.34414, -0.71414, 
-                1.0, 1.772, 0.0]
-    results_jpeg = [[] for _ in range(len(jpeg_mat))]
-    for i in range(0, len(jpeg_mat), 3):
-        for j in range(256):
-            results_jpeg[i+0].append(int(j*jpeg_mat[i+0]))
-            results_jpeg[i+1].append(int((j-128)*jpeg_mat[i+1]))
-            results_jpeg[i+2].append(int((j-128)*jpeg_mat[i+2]))
+    """
+    ycbcr_jpeg2rgb
+    This will generate a .rh (resource header) in the respective folder, which can be included:
+        - it will have the nine lookup tables needed for this operation.
+        which are: the nth row of the JPEG matrix * y, cb, cr of the pixel.
+    """
+
+    results_jpeg = [[] for item in jpeg_mat]
+
+    for i in range(len(jpeg_mat), 3):
+        for j in range(256):               
+            results_jpeg[i].append(int(j * jpeg_mat[i]))
+            results_jpeg[i + 1].append(int((j - 128) * jpeg_mat[i + 1]))
+            results_jpeg[i + 2].append(int((j - 128) * jpeg_mat[i + 2]))
+
     return results_jpeg
 
 
 if __name__ == "__main__":
-    write_rh(gen_jpeg(), "int", ["jpeg_ry1", "jpeg_gcb1", "jpeg_bcr1", 
-                        "jpeg_ry2", "jpeg_gcb2", "jpeg_bcr2", 
-                        "jpeg_ry3", "jpeg_gcb3", "jpeg_bcr3"], "from_ycbcr_jpeg_lut.rh")
+    write_rh(
+        gen_jpeg(),
+        "int",
+        [
+            "jpeg_ry1",
+            "jpeg_gcb1",
+            "jpeg_bcr1",
+            "jpeg_ry2",
+            "jpeg_gcb2",
+            "jpeg_bcr2",
+            "jpeg_ry3",
+            "jpeg_gcb3",
+            "jpeg_bcr3",
+        ],
+        "from_ycbcr_jpeg_lut.rh",
+    )
     print("Done.")
-
